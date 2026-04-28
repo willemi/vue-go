30af9162128cf404fe9a98313d0408c866a00a80
wangweibin1 <wangweibin1@360.cn>
2026-04-28 21:08:17 +0800

    ffeat;解决导航图标问题

--- demo/fullstack-training/frontend/my-admin/src/main.ts
+++ demo/fullstack-training/frontend/my-admin/src/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import "./style.css";
 
 const app = createApp(App);
@@ -11,5 +12,10 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(ElementPlus);
+
+// 全局注册所有 Element Plus 图标组件，使 <component :is="iconName" /> 可以通过字符串名称动态渲染
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
 
 app.mount("#app");
